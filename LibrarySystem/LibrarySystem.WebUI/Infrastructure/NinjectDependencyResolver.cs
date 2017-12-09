cdb5bdd3bbd7f5641f90d5a39afbb5637d6f4b1f
Yangzxcc2 <yangzxcc2@gmail.com>
2017-12-09 22:50:17 +0700

    Create Mock
    - bind IBooksRepository

--- LibrarySystem/LibrarySystem.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ LibrarySystem/LibrarySystem.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -2,10 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using LibrarySystem.Domain.Abstract;
+using LibrarySystem.Domain.Entities;
+using Moq;
+using Ninject;
 
 namespace LibrarySystem.WebUI.Infrastructure
 {
-    public class NinjectDependencyResolver
+    public class NinjectDependencyResolver : IDependencyResolver
     {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+        private void AddBindings()
+        {
+            Mock<IBooksRepository> mock = new Mock<IBooksRepository>();
+            mock.Setup(m => m.book).Returns(new List<Book>
+            {
+                new Book {BookName = "Percy Jackson", BookID = 1},
+                new Book {BookName = "Prince of Persia", BookID = 2},
+                new Book {BookName = "Divergent", BookID = 3}
+            });
+            kernel.Bind<IBooksRepository>().ToConstant(mock.Object);
+        }
     }
 }
